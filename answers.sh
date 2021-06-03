c6d2bda108b8eb216fa69b1b0197a4a3bb60958d
abdulrhman soukarieh <abdulrhman.soukarieh1@gmail.com>
2021-06-03 23:50:05 +0300

    Joins

--- answers.sh
+++ answers.sh
@@ -30,3 +30,15 @@ Question-1: INSERT INTO graduates ('name ' , Age , Gender , Points )
 Question-2: UPDATE graduates SET Graduation = '8/9/2018' WHERE name= 'Layal';
 
 Question-3: DELETE FROM students WHERE name = 'Layal';
+
+
+Joins 
+
+Question-1 : SELECT e.name , c.name , c.Date  FROM employees e LEFT JOIN companies c ON e.Company = c.name;
+
+Question-2: SELECT e.name FROM employees e LEFT JOIN companies c ON e.Company = c.name WHERE c.Date < 2000;
+
+Question-3: SELECT DISTINCT c.name FROM companies c INNER JOIN employees e ON e.Company = c.name WHERE e.Role = 'Graphic Designer';
+
+
+
